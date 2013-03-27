814621505d89301edb86e64b73dda664c57b81e8
Shveta <shvetasingh92@gmail.com>
2013-03-27 11:58:19 +0530

    demo

---
+++ CATapp/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace CATapp
+{
+    public partial class MainPage : PhoneApplicationPage, INotifyPropertyChanged
+    {
+        // Data context for the local database
+        private CatAppDataContext catAppDB;
+        public static string DBConnectionString = "Data Source=isostore:/catappdb.sdf";
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            // Connect to the database and instantiate data context.
+            catAppDB = new CatAppDataContext(DBConnectionString);
+
+            // Data context and observable collection are children of the main page.
+            this.DataContext = this;
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify the app that a property has changed.
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        // Define an observable collection property that controls can bind to.
+        private ObservableCollection<Test> _tests;
+        public ObservableCollection<Test> Tests
+        {
+            get
+            {
+                return _tests;
+            }
+            set
+            {
+                if (_tests != value)
+                {
+                    _tests = value;
+                    NotifyPropertyChanged("Tests");
+                }
+            }
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            // Define the query to gather all of the to-do items.
+            var testsInDB = from Test test in catAppDB.Tests 
+                            select test;
+
+            // Execute the query and place the results into a collection.
+            Tests = new ObservableCollection<Test>(testsInDB);
+
+            // Call the base method.
+            base.OnNavigatedTo(e);
+        }
+    }
+}
